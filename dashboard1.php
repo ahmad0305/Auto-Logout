3e2b6fd02a2ba5b866d797244fa20c981644c325
ahmad0305 <96977081+ahmad0305@users.noreply.github.com>
2023-07-25 00:16:18 +0530

    Add files via upload

---
+++ dashboard1.php
@@ -0,0 +1,26 @@
+<?php
+require("user_auth.php");
+echo "Hello";
+?>
+<a href="dashboard.php">Dashboard</a><br/>
+<a href="logout.php">Logout</a>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+setInterval(function(){
+	check_user();
+},2000);
+function check_user(){
+	jQuery.ajax({
+		url:'user_auth.php',
+		type:'post',
+		data:'type=ajax',
+		success:function(result){
+			if(result=='logout'){
+				window.location.href='logout.php';
+			}
+		}
+		
+	});
+}
+</script>
